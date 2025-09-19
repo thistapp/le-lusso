6e2f5611ee3a604fa59837e91310908423f85f69
thistapp <58948210+thistapp@users.noreply.github.com>
2025-09-19 16:24:08 +0700

    fix config next intl'

--- frontend/src/i18n/routing.ts
+++ frontend/src/i18n/routing.ts
@@ -3,7 +3,8 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
     locales: ['en', 'th'],
     defaultLocale: 'th',
-    localePrefix: 'as-needed',
+    // Optional: hide the default locale in the URL
+    // localePrefix: 'as-needed',
     pathnames: {
         '/': '/',
         '/pathnames': {},
